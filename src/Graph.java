1317b6424c15320758ea8017c192b3231ecf5c56
melindar <melindar@rams.colostate.edu>
2014-12-01 22:07:20 -0700

    Added Graph class to the project.

---
+++ src/Graph.java
@@ -0,0 +1,25 @@
+// PA5
+// Author: Melinda Ryan
+// Date:   12/1/2014
+// Class:  CS200
+// Email:  melindar@rams.colostate.edu
+
+public class Graph 
+{
+	
+	//Returns the number of edges incoming to a particular filename.
+	public int inDegree(String filename)
+	{
+		return 0;
+	}
+	
+	/*Creates a file that specifies the graph and can be printed by the dot program. 
+	 * The outputFile should have the extension ".dot" added to it
+	 */
+	public void writeDotFile(String outputFile)
+	{
+		
+	}
+    
+    
+}
