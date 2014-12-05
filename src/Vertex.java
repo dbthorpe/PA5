7b85ddb1140aaee5d23d1ac6f447deb63d1831a5
melindar <melindar@rams.colostate.edu>
2014-12-04 20:56:02 -0700

    Comment in Vertex to force merge.

--- src/Vertex.java
+++ src/Vertex.java
@@ -58,6 +58,7 @@ public class Vertex
 		nextVertex.weight = nextWeight;
 	}
 	
+	//Two vertices are equal if their filename are the same
 	public boolean equals(Object other)
 	{
 		if(other instanceof Vertex)
